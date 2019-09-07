2a00b4e3fa92606fb19422be3ecabcb106bd6b3b
Naguib Ihab <nick@upilio.com>
2019-09-08 09:08:25 +1000

    wip  ivi skill level

---
+++ api/data/ivi-skill-level.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    Level:	3,
+    ANZSCO_CODE:	12,
+    Title:	"Farmers and farm managers",
+    State: "NSW",
+    19: {
+      Jan: 128.633,
+      Feb: 130.827,
+      Mar: 132.737,
+      Apr: 134.384,
+      May: 135.711,
+      Jun: 136.702,
+      Jul: 137.449
+    }
+  }
+]
